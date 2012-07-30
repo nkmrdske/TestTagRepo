0b74b7b5d44f8790ea05d6f744badeb9fcdc9b42
nkmr_dske <nakamura_d@mti.co.jp>
2012-07-30 16:34:21 +0900

    first

---
+++ gen/nkmr/dske/test/gittag/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package nkmr.dske.test.gittag;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/nkmr/dske/test/gittag/TestGitTagActivity.java
@@ -0,0 +1,15 @@
+package nkmr.dske.test.gittag;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestGitTagActivity extends Activity {
+
+	/* 1st*/
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
